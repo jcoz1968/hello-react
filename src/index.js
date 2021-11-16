602325a97c5b3f3f8e742e046632e4885c7f3973
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2021-11-16 12:33:49 -0600

    playground

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let city = {
-  name: 'Moscow',
-  country: 'Russia'
+
+function Hello() {
+  return (
+    <div>
+      <h1>Welcome to React!</h1>
+      <p>Let's build something cool.</p>
+    </div>
+  )
 }
 
+// let city = {
+//   name: 'Moscow',
+//   country: 'Russia'
+// }
+
 ReactDOM.render(
-  <h1 id='heading' className='cool-text'>Hello from {city.name} is in {city.country} !</h1>,
+  <Hello />,
+  document.getElementById('root')
+
+  // <h1 id='heading' className='cool-text'>Hello from {city.name} is in {city.country} !</h1>,
   // <ul>
   //   <li>Hot Dogs</li>
   //   <li>Hamburgers</li>
@@ -21,7 +34,6 @@ ReactDOM.render(
   //   React.createElement('li', null, 'Hamburgers'),
   //   React.createElement('li', null, 'Pizza')
   // ),
-  document.getElementById('root')
 );
 
 
