682fae94f86b5dafc4b43d83ecfa915d5fa8de0b
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2021-11-16 12:29:13 -0600

    more playground

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let city = {
+  name: 'Moscow',
+  country: 'Russia'
+}
 
 ReactDOM.render(
-  <ul>
-    <li>Hot Dogs</li>
-    <li>Hamburgers</li>
-    <li>Pizza</li>
-    <li>Sushi</li>
-  </ul>,
+  <h1 id='heading' className='cool-text'>Hello from {city.name} is in {city.country} !</h1>,
+  // <ul>
+  //   <li>Hot Dogs</li>
+  //   <li>Hamburgers</li>
+  //   <li>Pizza</li>
+  //   <li>Sushi</li>
+  // </ul>,
   // React.createElement('ul', 
   //   {style: { color: 'blue' }}, 
   //   React.createElement('li', null, 'Hot Dogs'),
