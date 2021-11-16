be5ab240020efddb620b019e4cc5bad3250059fd
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2021-11-16 12:16:02 -0600

    dom stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ul>
+    <li>Hot Dogs</li>
+    <li>Hamburgers</li>
+    <li>Pizza</li>
+    <li>Sushi</li>
+  </ul>,
+  // React.createElement('ul', 
+  //   {style: { color: 'blue' }}, 
+  //   React.createElement('li', null, 'Hot Dogs'),
+  //   React.createElement('li', null, 'Hamburgers'),
+  //   React.createElement('li', null, 'Pizza')
+  // ),
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
